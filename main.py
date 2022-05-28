62b91b57f16579323c7067e0279fa66f3a671e4d
SunGunPark <44153017+SunGunPark@users.noreply.github.com>
2022-05-28 17:07:24 +0900

    상관관계 확인 및 추가 EDA 작업

--- main.py
+++ main.py
@@ -12,20 +12,46 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+from datetime import datetime
 import preprocessing as pp
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+import seaborn as sns
+
+font_path = "C:/Windows/Fonts/H2GTRM.TTF"
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font)
 
 employ_data = pp.load_csv('고용수준실적SBHI')
 business_data = pp.load_csv('경기전반실적SBHI')
 domestic_data = pp.load_csv('내수판매실적SBHI')
 export_data = pp.load_csv('수출실적SBHI')
-print(export_data)
+fund_data = pp.load_csv('자금사정실적SBHI')
+sales_data = pp.load_csv('영업이익실적SBHI')
+esi_data = pp.load_csv('경제심리지수')
+#print(esi_data)
 
 general_data = pp.join_data(business_data,employ_data,"고용수준실적SBHI")
 general_data = pp.join_data(general_data,domestic_data,"내수판매SBHI")
 general_data = pp.join_data(general_data,export_data,"수출실적SBHI")
+general_data = pp.join_data(general_data,fund_data,"자금사정실적SBHI")
+general_data = pp.join_data(general_data,sales_data,"영업이익실적SBHI")
+general_data = pp.create_nan_data(general_data)
+general_data = pp.delete_nan_data(general_data)
+general_data = pp.change_datetime(general_data)
 print(general_data)
+general_data['수출실적SBHI'] = general_data['수출실적SBHI'].astype(float)
+print(general_data.corr())
+
+print(general_data.info)
+# general_data.plot(x='시점')
+
+# pp.save_df_to_csv(general_data,"최종데이터")
 
 # general_data.to_csv("data/최종데이터.csv")
+
+plt.scatter(general_data['시점'],general_data['수출실적SBHI'])
+plt.show()
 
 x_train = []
 x_valid = []
--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,35 @@
 import pandas as pd
+import numpy as np
+from datetime import datetime
+
+
+
 def load_csv(csv_name):
     data = pd.read_csv(f'data/{csv_name}.csv', encoding='euc-kr')
+    return data
+
+def create_nan_data(df):
+    data = df.replace('-',np.NaN)
+    return data
+
+def delete_nan_data(df):
+    data = df
+    #data = df.fillna(method="bfill")
     return data
 
 def join_data(df1,df2,dataName):
     data = pd.concat([df1,df2[f'{dataName}']],axis=1)
     return data
+
+def join_data_2(df1,df2,dataName):
+    data = pd.concat([df1,df2])
+    return data
+
+def save_df_to_csv(df,fileName):
+    path = 'C:/Users/park/Documents/GitHub/biz_predict/data/'
+    df.to_csv(f'{path}{fileName}.csv',encoding='euc-kr',mode="w")
+    print('csv 저장 완료')
+
+def change_datetime(df):
+    #df['시점'] = pd.to_datetime(df['시점'],format="%m%Y")
+    return df
