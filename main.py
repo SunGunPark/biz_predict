931c0af8c90f76bc6e16792c1cdb3bd53e5053d8
SunGunPark <44153017+SunGunPark@users.noreply.github.com>
2022-06-04 13:33:53 +0900

    2022-06-04 데이터 연별, 계절별, 월별 나눔

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from sklearn.metrics import accuracy_score
 from datetime import datetime
 import preprocessing as pp
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from matplotlib import font_manager, rc
 import seaborn as sns
 
@@ -29,29 +30,51 @@ export_data = pp.load_csv('수출실적SBHI')
 fund_data = pp.load_csv('자금사정실적SBHI')
 sales_data = pp.load_csv('영업이익실적SBHI')
 esi_data = pp.load_csv('경제심리지수')
+all_data = pp.load_csv('데이터셋')
+rm_data = pp.load_csv('원자재_수입동향')
 #print(esi_data)
 
 general_data = pp.join_data(business_data,employ_data,"고용수준실적SBHI")
 general_data = pp.join_data(general_data,domestic_data,"내수판매SBHI")
-general_data = pp.join_data(general_data,export_data,"수출실적SBHI")
+#general_data = pp.join_data(general_data,export_data,"수출실적SBHI")
 general_data = pp.join_data(general_data,fund_data,"자금사정실적SBHI")
 general_data = pp.join_data(general_data,sales_data,"영업이익실적SBHI")
 general_data = pp.create_nan_data(general_data)
 general_data = pp.delete_nan_data(general_data)
 general_data = pp.change_datetime(general_data)
-print(general_data)
-general_data['수출실적SBHI'] = general_data['수출실적SBHI'].astype(float)
-print(general_data.corr())
+#print(general_data.corr())
 
-print(general_data.info)
+#print(general_data.info)
 # general_data.plot(x='시점')
 
-# pp.save_df_to_csv(general_data,"최종데이터")
+general_data['시점'] = general_data['시점'].astype(str)
+all_data['시점'] = all_data['시점'].astype(str)
+rm_data['시점'] = rm_data['시점'].astype(str)
+all_data = pp.join_data_2(all_data,rm_data)
+all_data.loc[all_data['시점']=='2015.1','시점']='2015.10'
+all_data.loc[all_data['시점']=='2016.1','시점']='2016.10'
+all_data.loc[all_data['시점']=='2017.1','시점']='2017.10'
+all_data.loc[all_data['시점']=='2018.1','시점']='2018.10'
+all_data.loc[all_data['시점']=='2019.1','시점']='2019.10'
+all_data.loc[all_data['시점']=='2020.1','시점']='2020.10'
+all_data.loc[all_data['시점']=='2021.1','시점']='2021.10'
+all_data['시점'] = pd.to_datetime(all_data['시점'])
+all_data['년'] = all_data['시점'].dt.year
+all_data['월'] = all_data['시점'].dt.month
+all_data['계절'] = np.select([(all_data['월'] <= 2),(all_data['월'] <= 5),
+                            (all_data['월'] <= 8),(all_data['월'] <= 11),
+                            (all_data['월'] == 12)],
+                           ['겨울','봄','여름','가을','겨울'], default=np.nan)
 
-# general_data.to_csv("data/최종데이터.csv")
+print(all_data)
 
-plt.scatter(general_data['시점'],general_data['수출실적SBHI'])
-plt.show()
+pp.save_df_to_csv(all_data,"최종데이터")
+
+
+#plt.plot(general_data['시점'],general_data['고용수준실적SBHI'])
+plt.scatter(all_data['시점'],all_data['고용수준실적SBHI'])
+plt.title('고용수준실적SBHI 산점도 그래프')
+#plt.show()
 
 x_train = []
 x_valid = []
--- preprocessing.py
+++ preprocessing.py
@@ -21,8 +21,8 @@ def join_data(df1,df2,dataName):
     data = pd.concat([df1,df2[f'{dataName}']],axis=1)
     return data
 
-def join_data_2(df1,df2,dataName):
-    data = pd.concat([df1,df2])
+def join_data_2(df1,df2):
+    data = pd.merge(df1,df2,on='시점',how='left')
     return data
 
 def save_df_to_csv(df,fileName):
@@ -32,4 +32,13 @@ def save_df_to_csv(df,fileName):
 
 def change_datetime(df):
     #df['시점'] = pd.to_datetime(df['시점'],format="%m%Y")
+    return df
+
+def create_season(df):
+    season = ['봄','여름','가을','겨울']
+    conditions = [ (3 <= df['월']) & (df['월'] <= 5), (6 <= df['월']) & (df['월'] <= 8),
+                  (9 <= df['월']) & (df['월'] <= 11), (12 <= df['월']) & (df['월'] <= 2)]
+    col = ['월']
+
+    df['계절'] = np.select(conditions,season,default=np.nan)
     return df
